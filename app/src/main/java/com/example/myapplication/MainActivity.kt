e9106484d108a638ac4bbcb9a72e907645b22865
webber <webber0971@gmail.com>
2022-08-16 11:14:18 +0800

    add activity_layout

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var insertButton: Button
+    private lateinit var deleteButton: Button
+    private lateinit var cleanButton: Button
+    private lateinit var updateButton: Button
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        insertButton=findViewById(R.id.insertButton)
+        deleteButton=findViewById(R.id.deleteButton)
+        cleanButton=findViewById(R.id.cleanButton)
+        updateButton=findViewById(R.id.updateButton)
+
+
     }
 
 }
